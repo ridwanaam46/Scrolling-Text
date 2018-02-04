bd4dff313552665983bc9c0233b34dfcc61bc8f8
Muhammad Ridwan <hansamu46@gmail.com>
2018-02-05 00:38:40 +0700

    first commit

---
+++ app/src/main/java/com/example/asusx453sa/scrollingtext/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.asusx453sa.scrollingtext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
